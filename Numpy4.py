17f7d51743f48b44c1bdbecf73f20b83550121d1
ASHWIN <email@example.com>
2026-01-26 14:13:30 +0530

    Add File

---
+++ Numpy4.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array_2d = np.array ([[1,2,3], [4,5,6], [7,8,9]])
+
+subarray = array_2d[0:2, 1:3]
+
+print(array_2d)
+print("Sliced Subarray:\n",subarray)
+
---
+++ Numpy5.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array_1d = np.array([10, 20, 30, 40, 50])
+array_reshaped = array_1d.reshape(1, 5)
+
+print(array_1d)
+print("Reshaped 2D Array:\n", array_reshaped)
+
+
