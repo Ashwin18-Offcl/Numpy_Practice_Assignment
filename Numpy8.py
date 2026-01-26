d8064fd86e4ec4b86cab942229019e23b078da71
ASHWIN <email@example.com>
2026-01-26 14:37:36 +0530

    Add File

---
+++ Numpy8.py
@@ -0,0 +1,9 @@
+import numpy as np
+array = np.array([[2,3,6],[8,2,9],[7,9,1]])
+
+
+array_sum = np.sum(array)
+array_mean = np.mean(array)
+array_max = np.max(array)
+
+print("element-wise sum:", array_sum)
