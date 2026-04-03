3485b60d378f367efaf90de8b329848c0bef41f3
ASHWIN <email@example.com>
2026-04-03 19:09:18 +0530

    add

---
+++ Numpy10.py
@@ -0,0 +1,5 @@
+import numpy as np
+arr = np.array([10,20,30,40,50])
+indices = [0, 2, 4]
+print(arr[indices])
+print(arr[indices])
---
+++ Numpy11.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+cube = np.random.rand(4,4,4)
+print(cube[...,0])
+
